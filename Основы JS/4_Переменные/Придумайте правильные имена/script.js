f209023f2f67f4fbcafe213074d124ec43d451d7
Ustinova Ekaterina <galateya2406@yandex.ru>
2022-12-20 22:29:59 +0400

    Добавлены задачи 1,4

---
+++ Основы JS/4_Переменные/Придумайте правильные имена/script.js
@@ -0,0 +1,6 @@
+"use strict"
+
+let OurPlanetName = 'The Earth';
+
+let CurentUserName = 'Ekaterina';
+
---
+++ Основы JS/4_Переменные/Работа с переменными/script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let name;
+name = "Джон";
+
+let admin;
+admin = name;
+
+alert (admin);
+
+
